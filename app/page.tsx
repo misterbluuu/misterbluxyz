612e98a527157067e1a701942222a2b7bc69a60d
misterbluuu <austgiordano@gmail.com>
2025-11-07 09:30:53 -0800

    about me update

--- app/page.tsx
+++ app/page.tsx
@@ -44,31 +44,31 @@ There is a lot of working hardware that goes to waste from companies just throwi
   <p>
     I’ve struggled for a while to find something that I felt like only I could do. My self confidence sort of dropped as I went through the classic pipeline of “gifted kid in primary school to absolute trainwreck in college.” My grades were dogwater. My study strategy was don’t go to class at all ever and then cram all of the content of a 12 week fluid dynamics course into a single 2 hour study session before the final. I always sort of knew I wasn’t built for school, but college was a wake up call like no other. It’s not like I didn’t want to learn — I really did — but sitting and listening to someone yap for 2 hours straight was equivalent to torture for me.
   </p>
-
+<p>.</p>
   <p>
     What did I spend my 4 years in college doing if not going to class, you may ask? Literally anything and everything else. I worked 2 jobs at a time (almost) my entire college career, I founded multiple clubs (and held officer positions in others), I went to networking events and conferences. I was consistently on campus 14 hours a day without ever stepping in a classroom. In any free time I had, I hung out off the clock at either of the 2 makerspaces I worked at on campus. I loved it there. The community, the machines, the serene sound of the 3D printers running through the night as I sat in the lab cramming class projects. It felt like home. I found a real passion for helping people take ideas from abstract concepts into real, tangible products. I put a lot of energy into other people’s dreams, and I loved every second of it. Though sometimes I thought of myself as that guy who “leads others to a treasure that I cannot possess.” Support class to the max. I always knew that I would eventually need to follow my own path, but I never thought the time to do that would come so soon.
   </p>
-
+<p>.</p>
   <p>
     I (barely) graduated this May, and by some miracle got a job. One week I had no idea what I was going to do over the summer for work, and literally the next I was 3,000 miles away from home in a hacker house of 18 people in Silicon Valley at a robotics start up. Anyone who knows me will tell you if there is something I am absolutely over the moon about to the point that I “light up” when talking about it, it’s robotics. I can’t exactly explain why I ended up fixated on robots specifically, but once I started diving into it, I got obsessed. I did multiple internships in high school related to robotics, became head coder of the robotics club, went full send in every way possible. It seemed like a dream come true to work at a company specializing in building the first generation of autonomous home robots right out of college.
   </p>
-
+<p>.</p>
   <p>
     I don’t entirely hate my current job (most of the time). I still get to build things, and I’m working full time as support for the engineering team, even if I’m not a “real” engineer. I really do think our product is (going to eventually be) good and that the company is doing cool stuff, but I just don’t feel that fire burning within me about it the way I did with other robotics stuff I did in college. This issue was made way worse after piloting the fighting robot for Silicon Colosseum and eventually helping Caeden do the first ever humanoid robot skydive. Building vacuum cleaner robots just doesn’t hit the same after that I fear.
   </p>
-
+<p>.</p>
   <p>
     During my time in Chicago for the skydive project, I was around a lot of people who went all in on their dreams. They found something they loved to do, and centered their entire life around it. I also jumped out of an airplane. Both of these experiences really got me thinking. If other people can go all in on their dreams as soon as they figure them out, why can’t I? Is doing something as insane as quitting my full time job to do what I truly love scarier than falling 13,500 ft? The conclusion I came to is that it is not (I’m sort of lying, skydiving was rad as hell and I wasn’t really scared of it at all but you get the point).
   </p>
-
+<p>.</p>
   <p>
     After I got back from the trip I did some serious pondering about what I wanted to do with my life. The first place I started was “what do I absolutely hate more than anything in the world that I think should be changed?”. The second was “what do I love doing?”. I was pretty quickly reminded of an idea I had a while ago that incorporated my answers to both of those questions. I was in the EE lab at work and we were trying to figure out what to do with a ton of electronics we didn’t have space for, and one of the engineers was like “when in doubt, throw it out”. To very expensive, still functioning hardware. This is not the first time I’ve seen this happen. I may or may not have done some dumpster diving a while ago for some really nice motors I knew still worked fine that were thrown out because the company just didn’t really have a use for them. Before I ended up swamped in other work I had been spending some time trying to figure out the logistics of donating these parts instead of just trashing them. The stuff that was thrown out wasn’t only one off prototyping parts, but a consistent stream of motors that failed quality checks due to noise but otherwise worked fine. I thought it might be cool to set up a system where we partner with a makerspace nearby that is designated as a nonprofit and donate stuff to them in return for government issued tax breaks.
   </p>
-
+<p>.</p>
   <p>
     That then developed into an idea of a website or app that partnered hardware companies with makerspaces or individuals. I told my fellow hacker housemates about it, and the first thing one of them said was “there’s no profit model for that”. Which was entirely accurate, at the time at least. Fun fact about me is that I’m not very money motivated when it comes to doing stuff (if I was, I definitely wouldn't be working where I work now LMAO). As long as I can afford rent and food and to go do Fun Things Out On the Town on occasion, I’m chilling. Either way, my intention was not to make money doing this. I was just pissed that so much good hardware was going to waste. In fact I was ready to put some of my own money into it to host the server or whatever (idfk how websites work (can you tell?)) was needed. I genuinely just wanted to support my maker community members and help alleviate the global waste crisis a little bit in the process.
   </p>
-
+<p>.</p>
   <p>
     I didn’t end up building the website at that point because work got insane (we love toxic start-up grind culture) and also a lot of things were happening in the hacker house and I wasn’t really in the right headspace to jump into it. I definitely kept thinking about it though. Surely there was a way to make it work.
   </p>
