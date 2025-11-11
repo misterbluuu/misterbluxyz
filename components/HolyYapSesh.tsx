3d60c6a7651ec13a74f1492887b7190c5327fb8d
misterbluuu <austgiordano@gmail.com>
2025-11-10 21:46:37 -0800

    update shiiii

--- components/HolyYapSesh.tsx
+++ components/HolyYapSesh.tsx
@@ -1,6 +1,15 @@
 export default function HolyYapSesh() {
   return (
-    <section className="relative z-10 max-w-4xl text-lg leading-relaxed font-display space-y-24">
+ <section className="relative z-10 max-w-4xl mx-auto text-lg leading-relaxed font-display space-y-24">
+      {/* MAIN TITLE */}
+      <div className="text-center mb-12">
+        <h2
+          className="text-xl md:text-2xl text-transparent bg-clip-text bg-gradient-to-r 
+                     from-cyan-400 via-emerald-400 to-blue-400 mt-4 font-[Chakra_Petch]"
+        >
+          my love letter to the art of making
+        </h2>
+      </div>
 
       <div className="section-box">
         <div className="section-title-sticker">About Me</div>
@@ -139,7 +148,44 @@ export default function HolyYapSesh() {
 
       <div className="section-box">
         <div className="section-title-sticker">Act I: The Small Stuff</div>
-        <p>*wip*</p>
+        <p>As much as I would love to immediately jump into facilitating a global hardware revolution, changing the
+             world can’t be done in a day. It needs to be a long and methodical process, so I’ve split up my Grand Plan
+              into “Acts”, focusing first on establishing a proof of concept in the most likely place for this all to work:
+               the San Francisco Bay Area. </p>
+<p>.</p>
+<p>I think the best way to start this project is to establish a repertoire with current start-ups/companies and makerspaces
+     locally when it comes to the one-off waste. My current start-up currently has a pretty serious random items issue. We
+      have an entire room dedicated to “just throwing” stuff in it. I think it might be a good idea to try reaching out to hardware
+       companies from the angle of “hey, you have a lot of random shit? We can take it off your hands, and clean up your space a 
+       little in the process”. Ideally, this would be a paid service of some sort, but cheap compared to hiring an actual cleaning 
+       service. Either way, it would be a mutually beneficial transaction that would allow the acquisition of hardware that would 
+       have likely just been thrown out otherwise. I also plan to try to advertise a way for individuals to donate hardware if they
+        feel so inclined. </p>
+<p>.</p>
+<p> The next step is taking what I do get and seeing what of it is actually usable. I am aware that distributing hardware deemed
+     by a company to be “trash” could potentially be a liability issue, but I will not let anything get passed on that’s not safe.
+      This is going to be a major pain point in the beginning I think, at least until I can establish a database of “here’s this motor 
+      and here’s how to check if it’s functional and safe”. I’m aware a non-negligible amount of hardware I acquire will likely be 
+      beyond repair and that I will need to find something to do with it. This is specifically important when it comes to batteries
+      , as they are hard to dispose of properly. I plan to extensively research the correct way to recycle/donate anything I might
+       end up with to minimize environmental impact. </p>
+<p>.</p>
+<p> Once I have a collection of good hardware, the next step is to figure out how to get it to people. My original idea was to make 
+    some sort of online Facebook Marketplace type thing specifically for hardware. The issue is I don’t want to be selling this. I
+     want whatever hardware I save to be as close to entirely free as possible. While I think the marketplace is definitely something
+      I plan to work on and try to figure out in the future, I think the first thing I can feasibly do is once I get enough 
+      hardware is host some hackathons with it in the Bay Area. While I am looking to find a way to get people individually passionate
+       about hardware, I also want to build a community of super passionate hardware enthusiasts, and what better way than a little 
+       friendly competition? People in the Bay like doing Fun Nerdy Things, so I think between my current network, some meetup/event
+        posting apps, and a bunch of silly posters put up in the downtown areas of a few cities I think it should be easy to garner
+         attention. </p>
+<p>.</p>
+<p> I also want to stick to my roots and try to figure out if there are makerspaces or STEM education programs in the area
+ that would benefit from recycled hardware. This would either be in the form of donations to an electronics lab, or hosting 
+ proper “hey we have a lot of these motors, lets teach kids how to build funny little machines out of them”. I found 
+ out through being an Outdoor School (5th grade environmental camp) Counselor that I love teaching kids about things
+  I’m passionate about and getting them excited about STEM, so this is something I could confidently do.
+</p>
       </div>
 
       <div className="section-box">
